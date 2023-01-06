ccaaa50385d56ddef7cd4cf11f78fc779c44aa7b
GuadoDex <juliandoudoule@gmail.com>
2023-01-06 17:17:15 +0100

    Corrections + OLED Display Support + Features test

--- firmware/main/main.ino
+++ firmware/main/main.ino
@@ -1,14 +1,25 @@
 #include <PCF8574.h>
+#include <Adafruit_SSD1306.h>
 
 #define INPUT_BOARD_ADDRESS 0x20       // 000 (@ bin)
 #define OUTPUT_BOARD_ADDRESS 0x21      // 001 (@ bin)
 #define INTERFACE_BOARD_ADDRESS 0x22   // 010 (@ bin)
-#define WATER_INFILTRATION_PIN 8      // Analog Water Infilitration Sensor
+#define OLED_DISPLAY_ADDRESS 0x3C      // 0x3C or 0x3D (! CAREFUL ! : Address on hardware =/= Address to use)
+#define WATER_INFILTRATION_PIN 8       // Analog Water Infilitration Sensor
+
+#define SDA 3   // GPIO 1
+#define SCL 4   // GPIO 2
+
+#define INT_1B 5         // Interrupt pin or Interface Board
+
+void IRAM_ATTR inter();
+bool interrupt = false;
 
 
-PCF8574 inputBoard(INPUT_BOARD_ADDRESS);
-PCF8574 outputBoard(OUTPUT_BOARD_ADDRESS);
-PCF8574 interfaceBoard(INTERFACE_BOARD_ADDRESS);
+PCF8574 inputBoard(INPUT_BOARD_ADDRESS, SDA, SCL);
+PCF8574 outputBoard(OUTPUT_BOARD_ADDRESS, SDA, SCL);
+PCF8574 interfaceBoard(INTERFACE_BOARD_ADDRESS, SDA, SCL, INT_1B, inter);
+Adafruit_SSD1306 oledDisplay(128, 64, &Wire, -1);
 
 
 void setup() {
@@ -16,7 +27,7 @@ void setup() {
   // Init serial link
   Serial.begin(9600);
   Serial.println(F("========================================================================"));
-  Serial.println(F("       VATHULT3000 v0.1 : Smart multi zones watering system             "));
+  Serial.println(F("          VATHULT3000 v0.1 : Smart multi zones watering system          "));
   Serial.println(F("========================================================================"));
   Serial.println("");
 
@@ -82,6 +93,25 @@ void setup() {
     //TODO: Proper error handling
     while(1);
   }
+
+  // Init OLED Display
+  Serial.print(F("OLED Display Init @ 0x"));
+  Serial.print(OLED_DISPLAY_ADDRESS, HEX);
+  Serial.print(F(" : "));
+  if (oledDisplay.begin(SSD1306_SWITCHCAPVCC, OLED_DISPLAY_ADDRESS)){
+    Serial.println(F("Success !"));
+  } else {
+    Serial.println(F("Failure..."));
+    Serial.println(F("Program shutdown"));
+    //TODO: Proper error handling
+    while(1);
+  }
+
+
+
+
+  // SCREEN TEST
+  oledDisplay.println("System : Opperational!");  // print() == Write; println() == Write and Line break at the end of the string
   
 
 }
@@ -96,4 +126,28 @@ void loop() {
     while(1);
   }
 
+
+  // Buttons test
+  if(interrupt){
+    interrupt = false;
+    if(!pcf.digitalRead(P0)){
+      Serial.println("RIGHT>>");
+    }
+    if(!pcf.digitalRead(P1)){
+      Serial.println("CENTER");
+    }
+    if(!pcf.digitalRead(P2)){
+      Serial.println("<<LEFT");
+    }
+  }
+  
 }
+
+
+
+void IRAM_ATTR inter() {
+  interrupt = true;
+  return;
+}
+
+
