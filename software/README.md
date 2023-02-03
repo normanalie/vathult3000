534668a262765905f53c241fd206ca35da6a9c3c
GuadoDex <juliandoudoule@gmail.com>
2023-02-03 23:47:06 +0100

    Register/Login/Logout working with DB

--- software/README.md
+++ software/README.md
@@ -4,5 +4,10 @@
    1. Add ```SESSION_SECRET=developpement_key``` in this file
    2. Save
 3. In a shell run  ```npm install -dev``` to install dependencies
-4. In a shell run ```node .``` to start the server
-5. Go to ```localhost:3000```
+4. Install ```XAMPP``` on your computer (https://www.apachefriends.org/fr/index.html)
+   1. Click on ```Start``` for ```Apache``` 
+   2. Click on ```Start``` for ```MySQL``` 
+   3. Click on ```Admin``` for ```MySQL``` to open phpMyAdmin
+5. Create a DB called ```vathult3000``` in phpMyAdmin and create the ```Users``` table by following the same structure in the ```db-structure.json``` file 
+6. In a shell run ```node .``` to start the server
+7. Go to ```localhost:3000```
--- software/server.js
+++ software/server.js
@@ -1,6 +1,9 @@
 if (process.env.NODE_ENV !== 'production') {   // Si on est en mode développement, utiliser le fichier .env pour variables de l'environnement
     require('dotenv').config();
+    console.log(`~(SERVER)~ [WARNING!] - Dev Mode enabled!`);
+    console.log(`~(SERVER)~ [Dev # i] - Website link : "localhost:3000"`);
 }
+
 
 const express = require('express');
 const app = express();
@@ -9,16 +12,82 @@ const passport = require('passport');
 const flash = require('express-flash');
 const session = require('express-session');
 const methodOverride = require('method-override');
+const mysql = require('mysql');
+const mysqlLogID = require('./mysql-log-id.json');
+const dbStructure = require('./db-structure.json');
+const LocalStrategy = require('passport-local').Strategy;
 
 
-const initializePassport = require('./passport-config');
-initializePassport(
-    passport,
-    email => users.find(user => user.email === email),
-    id => users.find(user => user.id === id)
-);
 
-const users = [];   // A remplacer par la base SQL pour les utilisateurs
+const db = mysql.createConnection({        // Init MySQL connection
+    host: mysqlLogID.devUser.host,
+    user: mysqlLogID.devUser.user,
+    password: mysqlLogID.devUser.password
+})
+
+
+db.connect(err => {                         // Connect to MySQL
+    if (err) {
+        throw err
+    }
+    console.log(`(MySQL) [i] - MySQL connection established`);
+})
+
+
+defaultDBSelection();      // Select default website DB 
+
+
+
+// Passport.JS
+
+const authentificateUser = async (email, password, done) => {
+    let user;
+    let post = {mail: email}
+    let sql = `SELECT * FROM ${dbStructure.defaultDB.Users.tableName} WHERE ${dbStructure.defaultDB.Users.mail} = '${email}'`;
+    db.query(sql, post, async (err, results) => {
+        if (err) {
+            console.log(`(MySQL) [ERROR!] - Email search failure`);     // Check if Default DB exists
+            return console.log(err);
+        } else if (results.length == 0) {
+            return done(null, false, { message: 'No user with that email'});
+        }
+
+        user = {id: results[0].id, name: results[0].name, passwordHash: results[0].password_hash};
+        
+        try {
+
+            if (await bcrypt.compare(password, user.passwordHash)) {
+                return done(null, user);
+            } else {
+                return done(null, false, { message: 'Incorrect password' });
+            }
+    
+        } catch (e) {
+            return done(e);
+        }
+    });
+}
+
+passport.use(new LocalStrategy ({ usernameField: 'email', passwordField: 'password' }, authentificateUser));
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser((id, done) => {
+
+    let post = {id: id}
+    let sql = `SELECT * FROM ${dbStructure.defaultDB.Users.tableName} WHERE ${dbStructure.defaultDB.Users.id} = '${id}'`;
+    db.query(sql, post, async (err, results) => {
+        if (err) {
+            console.log(`(MySQL) [ERROR!] - ID search failure`);     // Check if Default DB exists
+            return console.log(err);
+        } else if (results.length == 0) {
+            return done(null, false);
+        }
+        return done(null, results[0]);
+    });
+});
+
+
+// [END] Passport.JS
 
 
 app.set('view-engine', 'ejs');
@@ -32,6 +101,12 @@ app.use(session( {
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(methodOverride('_method'));
+
+
+console.log(`~(SERVER)~ [BOOT] - Server started`)
+
+
+
 
 
 
@@ -48,7 +123,6 @@ app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
     successRedirect: '/dashboard',
     failureRedirect: '/login',
     failureFlash: true
-
 }))
 
 
@@ -68,24 +142,37 @@ app.post('/register', checkNotAuthenticated, async (req, res) => {
 
     try {
         const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        });
-        res.redirect('/login');
+
+        let columns = `${dbStructure.defaultDB.Users.mail}, ${dbStructure.defaultDB.Users.name}, ${dbStructure.defaultDB.Users.passwordHash}, ${dbStructure.defaultDB.Users.active}, ${dbStructure.defaultDB.Users.activateUID}`
+        let values = `'${req.body.email}', '${req.body.name}', '${hashedPassword}', '0', 'abcd1234'`; 
+        let sql = `INSERT INTO ${dbStructure.defaultDB.Users.tableName} (${columns}) VALUES (${values})`;
+        db.query(sql, (err) => {
+            if (err) {
+                console.log(`(MySQL) [ERROR!] - Unable to register new user`);
+                console.log(err);
+                res.redirect('/register');
+            } else {
+                console.log(`(MySQL) [i] - New user successfully registered`);
+                res.redirect('/login');
+            }
+        })
+
     } catch {
         res.redirect('/register');
     }
 })
 
-app.post('/validate/<user_id>/<uid>', (req, res) => {      // EMPTY [A programmer]
+app.post('/send-confirmation-mail', checkNotAuthenticated, (req, res) => {  // EMPTY [A programmer]
+    // Send a confirmation mail
+})
 
+app.post('/validate/:user-id/:uid', (req, res) => {      // EMPTY [A programmer]
+    // Set user's "active" state to "true" in DB
 })
 
 app.get('/dashboard', checkAuthenticated, (req, res) => {
     res.render('dashboard.ejs', {name: req.user.name} );
+    //console.log(req.user);
 })
 
 
@@ -93,16 +180,28 @@ app.get('/devices', checkAuthenticated, (req, res) => {
     res.render('devices.ejs');
 })
 
+app.get('/new-device', checkAuthenticated, (req, res) => {
+    res.render('new-device.ejs');
+})
+
+app.get('/save-new-device', checkAuthenticated, (req, res) => {  // EMPTY [A programmer]
+    // Save new device in DB. Set inputs/outputs to "null" (await device's real state)
+})
+
+app.post('/cancel-new-device', checkAuthenticated, (req, res) => {
+    res.redirect('devices.ejs');
+})
+
 app.get('/device/1', checkAuthenticated, (req, res) => {   // Remplacer le "1" par une variable qui s'adapte en en fonction de l'ID de l'appareil 
     res.render('device-info.ejs');
 })
 
-app.post('/device/<device_id>/setoutput/<output_id>/<state>', checkAuthenticated, (req, res) => {  // EMPTY [A programmer par rapport à l'ESP]
-
+app.post('/device/:device-id>/setoutput/:output-id/:state', checkAuthenticated, (req, res) => {  // EMPTY [A programmer par rapport à l'ESP]
+    // Set device output state
 })
 
-app.post('/device/<device_id>/setsource/<source>', checkAuthenticated, (req, res) => {             // EMPTY [A programmer par rapport à l'ESP]
-
+app.post('/device/:device-id/setsource/:source', checkAuthenticated, (req, res) => {             // EMPTY [A programmer par rapport à l'ESP]
+    // Set device water source
 })
 
 
@@ -121,6 +220,36 @@ function checkNotAuthenticated(req, res, next) {
     }
 
     next();
+}
+
+function defaultDBSelection() {
+    let sql = `USE ${dbStructure.defaultDB.dbName}`;
+    db.query(sql, (err) => {
+        if (err) {
+            console.log(`(MySQL) [ERROR!] - Default database "${dbStructure.defaultDB.dbName}" not found`);     // Check if Default DB exists
+            throw err;
+        }
+        console.log(`(MySQL) [i] - Default database "${dbStructure.defaultDB.dbName}" selected`);
+    });
+
+    sql = `SELECT * FROM ${dbStructure.defaultDB.Users.tableName}`                                             // Check if table "Users" in Default DB exists
+    db.query(sql, (err) => {
+        if (err) {
+            console.log(`(MySQL) [ERROR!] - Table "${dbStructure.defaultDB.Users.tableName}" in "${dbStructure.defaultDB.dbName}" doesn't exist`);
+            throw err;
+        }
+        console.log(`(MySQL) [i] - Table "${dbStructure.defaultDB.Users.tableName}" in "${dbStructure.defaultDB.dbName}" found`);
+    });
+
+    /*sql = `SELECT * FROM ${dbStructure.defaultDB.Vathults.tableName}`                                         // Check if table "Vathults" in Default DB exists
+    db.query(sql, (err) => {
+        if (err) {
+            console.log(`(MySQL) [ERROR!] - Table "${dbStructure.defaultDB.Vathults.tableName}" in "${dbStructure.defaultDB.dbName}" doesn't exist`);
+            throw err;
+        }
+        console.log(`(MySQL) [i] - Table "${dbStructure.defaultDB.Vathults.tableName}" in "${dbStructure.defaultDB.dbName}" found`);
+    });*/
+
 }
 
 app.listen(3000);  // Voir le site sur "localhost:3000"
